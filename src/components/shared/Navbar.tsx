bce18629981c83a26782c0a20e74a0f144d7c079
Nihar Hegde <niharhegde163@gmail.com>
2023-08-31 10:43:11 +0530

    addin navbar component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
+import { buttonVariants } from "../ui/button";
 
 const Navbar = () => {
   return (
     <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
-      <div>Login</div>
+      <div className="container flex items-center justify-between">
+        <Link href={"/"}>Logo</Link>
+        <Link
+          href={"signin"}
+          className={buttonVariants({ variant: "default" })}
+        >
+          Sign In
+        </Link>
+      </div>
     </div>
   );
 };
