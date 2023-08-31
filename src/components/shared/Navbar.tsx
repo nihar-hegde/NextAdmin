1a5311982d34ed77e574e3e0cdc319a16fbd823e
Nihar Hegde <niharhegde163@gmail.com>
2023-08-31 10:35:20 +0530

    first commit

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <div>Login</div>
+    </div>
+  );
+};
+
+export default Navbar;
