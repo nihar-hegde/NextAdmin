3b20ac71ca57b10bb35a235bd82254bb5fa6ad51
Nihar Hegde <niharhegde163@gmail.com>
2023-08-31 11:34:25 +0530

    added shadcn form

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,7 +1,52 @@
+"use client";
 import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userSignInSchema } from "@/lib/validators/signinValidators";
+import { z } from "zod";
+import { Button } from "../ui/button";
 
 const SignInForm = () => {
-  return <div>SignInForm</div>;
+  const form = useForm<z.infer<typeof userSignInSchema>>({
+    resolver: zodResolver(userSignInSchema),
+  });
+  function onSubmit(data: z.infer<typeof userSignInSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignInForm;
