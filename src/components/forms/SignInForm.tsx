3a43ec1be8b5b94962f4ce5d24b9d74547e763bf
Nihar Hegde <niharhegde163@gmail.com>
2023-08-31 11:52:00 +0530

    minor changes

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -26,24 +26,40 @@ const SignInForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="E-Mail" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button className="w-full mt-7" type="submit">
+          Sign In
+        </Button>
       </form>
     </Form>
   );
--- src/lib/validators/signinValidators.ts
+++ src/lib/validators/signinValidators.ts
@@ -2,7 +2,9 @@ import {z} from 'zod'
 
 
 export const userSignInSchema = z.object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
+    email: z.string().min(1, {
+      message: "E-mail is required.",
+    }).email(),
+    password:z.string().min(1,{message:"Password is required"})
+    
   })
