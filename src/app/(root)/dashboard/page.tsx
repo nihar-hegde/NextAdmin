3211acf35ab34b602b0e8e823c465df7f0ea00a3
Nihar Hegde <niharhegde163@gmail.com>
2023-09-01 19:36:14 +0530

    adding product page

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import PageLinkCard from "@/components/cards/PageLinkCard";
 import React from "react";
 
 const DashBoard = () => {
-  return <div className="h-screen flex flex-col justify-center items-center">Dashboard</div>
+  return <div className="h-screen flex flex-col justify-center items-center">
+    <PageLinkCard/>
+  </div>
 };
 
 export default DashBoard;
