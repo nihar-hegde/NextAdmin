8386cb1dd507e5c6a3b63bf061388ffcfc3725f8
Nihar Hegde <niharhegde163@gmail.com>
2023-09-01 11:09:22 +0530

    minor bug in signup form

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -104,7 +104,7 @@ const SignUpForm = () => {
       </div>
       <p className="text-center text-sm text-gray-600 mt-2">
         If you already have an account, Please&nbsp;
-        <Link href={"/signup"} className="text-blue-500 hover:underline">
+        <Link href={"/signin"} className="text-blue-500 hover:underline">
           Sign In
         </Link>
       </p>
